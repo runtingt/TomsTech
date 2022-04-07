123eda6c9b13f68631608ec5688c416a2417fbc6
Tom <tomrunting@googlemail.com>
2022-04-07 13:45:27 +0100

    Handle the server's response

--- sketch.js
+++ sketch.js
@@ -34,15 +34,14 @@ function stopTouchScrolling(canvas){
         if (e.target == canvas) {
             e.preventDefault();
         }
-    }, { passive: false });
-    
+    }, { passive: false }); 
 }
 
 // Setup the canvas
 function setup() {
     // Create a 400px by 400px blank canvas and center align it
     canvas = createCanvas(400, 400).id("drawingCanvas");
-    stopTouchScrolling(document.getElementById('drawingCanvas'));
+    stopTouchScrolling(document.getElementById("drawingCanvas"));
     centerCanvas();
     background(255, 255, 255);
     frameRate(60);
@@ -51,14 +50,13 @@ function setup() {
     reset_button = createButton("Reset");
     reset_button.id("controls");
     reset_button.size(200);
-    predict_button = createButton("Save");
+    predict_button = createButton("Predict");
     predict_button.id("controls");
     predict_button.size(200);
     alignButtons();
 
     reset_button.mousePressed(resetCanvas);
-    predict_button.mousePressed(saveImage);
-
+    predict_button.mousePressed(predictImage);
 }
 
 // Reset the canvas
@@ -67,21 +65,42 @@ function resetCanvas() {
     centerCanvas();
     background(255, 255, 255);
     frameRate(60);
+    document.getElementById("prediction").innerHTML = "I think this is a:"
 }
 
-// Save the image so we can predict information about it later
-function saveImage() {
-    // First, convert to a base64 string and print it to the console
-    let imageBase64String = canvas.elt.toDataURL();
-    console.log(imageBase64String);
+// Get the prediction returned by the server
+async function getPrediction(dataURL) {
+    // Send and await a response
+    let response = await fetch("http://129.151.91.21:5000/predict?Base64String=".concat(encodeURIComponent(dataURL)));
+    let text = await response.text();
 
-    // Test a GET request
-    fetch("129.151.91.21:5000/?Base64String=test")
+    // Log to console
+    console.log(text);
+
+    // Display on the screen
+    document.getElementById("prediction").innerHTML = "I think this is a".concat(String(text))
+}
+
+
+// Save the image so we can predict information about it later
+function predictImage() {
+    // Create a blank graphi
+    let resized = createGraphics(28, 28);
+
+    // Draw and scale the canvas content 
+    resized.image(canvas, 0, 0, 28, 28);
+    resized.loadPixels();
+
+    // Display the output in the top left corner
+    //image(resized, 0, 0);
+
+    // Send a GET request
+    getPrediction(String(resized.elt.toDataURL()));
 }
 
 // Draw black circles if the mouse is pressed
 function draw() {
-    strokeWeight(40);
+    strokeWeight(50);
     if(mouseIsPressed) {
         fill(0);
         line(mouseX, mouseY, pmouseX, pmouseY);
